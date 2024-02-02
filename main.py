92e0c938cef6626a1e2572fea1b43b7078d75c79
stiven <noemal@list.ru>
2024-02-02 16:24:02 +0300

    main.py добавлено: тестовая загрузка файла солнечной активности, "тестовая часть вывода кореляции"

--- main.py
+++ main.py
@@ -17,8 +17,33 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.input_button_sun.clicked.connect(self.update_sun_activity)
 
     filepath_txt = ["", ""]
+    filepath_sun_txt = ["", ""]
     successful_updated = False
 
+    import matplotlib.pyplot as plt
+    data_list_db_X = []
+    data_list_db_Y = []
+
+    data_list_sun_X = []
+    data_list_sun_Y = []
+
+    def calculate_correlation(list1, list2):
+        if len(list1) != len(list2):
+            raise ValueError("Списки должны иметь одинаковую длину.")
+
+        # Вычисляем среднее значение для каждого списка
+        mean1 = sum(list1) / len(list1)
+        mean2 = sum(list2) / len(list2)
+
+        # Вычисляем числитель и знаменатель для корреляции
+        numerator = sum((x - mean1) * (y - mean2) for x, y in zip(list1, list2))
+        denominator1 = sum((x - mean1) ** 2 for x in list1)
+        denominator2 = sum((y - mean2) ** 2 for y in list2)
+
+        # Вычисляем коэффициент корреляции
+        correlation = numerator / (denominator1 ** 0.5 * denominator2 ** 0.5)
+
+        return [correlation] * len(list1)
 
 
     def choose_txt(self):
@@ -28,18 +53,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         lists_num = 0
         A_list = 0
         B_list = 1
-        data_list_X = []
-        data_list_Y = []
         for i in range(1, lists[lists_num].max_row + 1):
-            data_list_X.append(lists[lists_num][i][A_list].value)
-            data_list_Y.append(lists[lists_num][i][B_list].value)
+            self.data_list_db_X.append(lists[lists_num][i][A_list].value)
+            self.data_list_db_Y.append(lists[lists_num][i][B_list].value)
 
-        data_list_X = list(map(float,data_list_X))
-        data_list_Y = list(map(float, data_list_Y))
+        self.data_list_db_X = list(map(float, self.data_list_db_X))
+        self.data_list_db_Y = list(map(float, self.data_list_db_Y))
 
         if (_filepath_txt[0] != ""):
             self.ui.file_path_window.setPlainText("Выбран файл мощности по пути: " + str(_filepath_txt))
-            self.set_db_graph(data_list_X, data_list_Y, False) # Нужно передавать судя данные из txt файла и алгоритма построения графика
+            self.set_db_graph(self.data_list_db_X, self.data_list_db_Y, False) # Нужно передавать судя данные из txt файла и алгоритма построения графика
             # self.set_sun_graph([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [34, 23, 34, 89, 12, 89, 12, 14, 100, 0], False) # Нужно передавать судя данные из txt файла и алгоритма построения графика
         else:
             self.ui.file_path_window.setPlainText("Файл мощности не выбран")
@@ -48,10 +71,33 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.compare_out(0, 0, True)
         self.filepath_txt = _filepath_txt
 
-
     def update_sun_activity(self):
         # Программа парсера погоды
         successful = True # Получена ли успешно погода?
+        _filepath_sun_txt = QFileDialog.getOpenFileName(self, str("Загрузить txt файл солнечной активности"), "/", str("TEXT (*.xlsx)"))
+
+        excel = openpyxl.open(_filepath_sun_txt[0], read_only=True)
+        lists = excel.worksheets
+        lists_num = 0
+        A_list = 0
+        B_list = 1
+        for i in range(1, lists[lists_num].max_row + 1):
+            self.data_list_sun_X.append(lists[lists_num][i][A_list].value)
+            self.data_list_sun_Y.append(lists[lists_num][i][B_list].value)
+
+        self.data_list_sun_X = list(map(float, self.data_list_sun_X))
+        self.data_list_sun_Y = list(map(float, self.data_list_sun_Y))
+
+        if (_filepath_sun_txt[0] != ""):
+            self.set_sun_graph(self.data_list_sun_X, self.data_list_sun_Y, False) # Нужно передавать судя данные из txt файла и алгоритма построения графика
+        else:
+            self.ui.file_path_window.setPlainText("Файл солнечной активности не выбран")
+            self.set_db_graph(0, 0, True)
+            # self.set_sun_graph(0, 0, True)
+            self.compare_out(0, 0, True)
+        self.filepath_sun_txt = _filepath_sun_txt
+
+        """"
         if (successful):
             self.set_sun_graph(0, 0, True)
             self.set_sun_graph([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [34, 23, 34, 89, 12, 89, 12, 14, 100, 0], False)
@@ -65,7 +111,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             msg.setWindowTitle("Получение солнечной активности")
             msg.exec_()
             self.successful_updated = False
-
+        """
     def compare_click(self):
         if (self.filepath_txt[0] == ""):
             msg = QMessageBox()
@@ -74,14 +120,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             msg.setInformativeText('Файл мощности (.txt) не был выбран!')
             msg.setWindowTitle("Сопостовленние данных")
             msg.exec_()
-        elif (self.successful_updated):
+        else:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Information)
             msg.setText("Успешно")
             msg.setInformativeText('Данные сопоставленны успешно')
             msg.setWindowTitle("Сопостовление данных")
             msg.exec_()
-            self.compare_out([1,2,3,4,5,6,7,8,9,10], [90,32,34,32,33,31,29,32,35,45], False) # Нужно передавать судя данные из алгоритма, которыми занимаются математики
+            self.compare_out(self.data_list_db_X, self.data_list_sun_Y, False) # Нужно передавать судя данные из алгоритма, которыми занимаются математики
+            print(self.data_list_db_X, self.data_list_sun_Y)
+        """
         else:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Critical)
@@ -89,10 +137,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             msg.setInformativeText('Не получена солнечная активность!')
             msg.setWindowTitle("Сопостовленние данных")
             msg.exec_()
-
+        """
     def compare_out(self, activity, power_db, clear): # Вывод графика зависимости солнечной активности от db
         if not(clear):
-            self.ui.graph_out.plot(activity, power_db)
+            self.ui.graph_out.plot(activity, power_db, pen=None, symbol='+')
         else:
             self.ui.graph_out.clear()
     def set_db_graph(self, time, db, clear): # Вывод графика зависимости времени от db
